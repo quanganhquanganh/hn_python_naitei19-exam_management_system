44d7e4fb022b4faf03b8494318ba2ad0a071b18b
bekeodangyeuqn <thaiduiqn@gmail.com>
2023-09-07 11:24:12 +0700

    App's authentication

--- exammanagement/main/static/javascript/script.js
+++ exammanagement/main/static/javascript/script.js
@@ -1,18 +1,10 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Toggle the side navigation
-  const sidebarToggle = document.body.querySelector("#sidebarToggle");
-  if (sidebarToggle) {
-    // Uncomment Below to persist sidebar toggle between refreshes
-    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-    //   document.body.classList.toggle('sb-sidenav-toggled');
-    // }
-    sidebarToggle.addEventListener("click", (event) => {
-      event.preventDefault();
-      document.body.classList.toggle("sb-sidenav-toggled");
-      localStorage.setItem(
-        "sb|sidebar-toggle",
-        document.body.classList.contains("sb-sidenav-toggled")
-      );
-    });
-  }
+jQuery(document).ready(function ($) {
+  $("#sidebarToggle").on("click", function (event) {
+    event.preventDefault();
+    $("body").toggleClass("sb-sidenav-toggled");
+  });
+  $(".close").on("click", function (event) {
+    event.preventDefault();
+    $(".message-toast").remove();
+  });
 });
