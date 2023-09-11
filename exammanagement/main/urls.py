f76bfa3c6440b74b57aec88c00c0119e4e167fd9
bekeodangyeuqn <thaiduiqn@gmail.com>
2023-09-11 09:34:25 +0700

    Exam detail

--- exammanagement/main/urls.py
+++ exammanagement/main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name='subjects'),
     path('subject/<int:pk>', views.SubjectDetailView.as_view(),
          name='subject-detail'),
+    path('chapter/<int:pk>', views.ChapterDetailView.as_view(),
+         name='chapter-detail'),
 ]
