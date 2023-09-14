dfb7b82baeea5addcd68c5d331b8600a3dc15640
bekeodangyeuqn <thaiduiqn@gmail.com>
2023-09-14 18:00:11 +0700

    Auto marks and display result

--- exammanagement/main/templatetags/keyvalue_action.py
+++ exammanagement/main/templatetags/keyvalue_action.py
@@ -5,3 +5,27 @@ register = template.Library()
 @register.filter
 def keyvalue(dict, key):
     return dict[key]
+
+
+@register.filter
+def in_category(things, category):
+    return things.filter(user=category)
+
+
+@register.filter
+def in_contain_more(things, right):
+    return things.filter(total_score__gte=right).values()
+
+
+@register.filter
+def in_progress(things, category):
+    pass_chapters = 0
+    for chapter in things.chapter_set.all():
+        if chapter.test_set.filter(user=category, total_score__gte=chapter.min_correct_ans).values():
+            pass_chapters += 1
+    return int((pass_chapters / things.chapter_set.all().count())*100)
+
+
+@register.filter
+def in_percent(left, right):
+    return 0 if right == 0 else int((left / right)*100)
